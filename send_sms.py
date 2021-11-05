810b11a3ea7ba9f85e778a5523dda8307db6a9ab
Dan Ferg <89812588+Dawafe@users.noreply.github.com>
2021-11-05 08:39:41 -0600

    ready to test

--- send_sms.py
+++ send_sms.py
@@ -6,13 +6,13 @@ from twilio.rest import Client
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 account_sid = os.environ['AC0f8a14d8a3f98dfd20a03453c51aba9b']
-auth_token = os.environ['']
+auth_token = os.environ['da65a8d2c3d7d15d95831dbd1dc2b93c']
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
                      body="SMS push weather service test",
-                     from_='+15017122661',
+                     from_='+18506018836',
                      to='+17145590485'
                  )
 
