30c80a635b39c3cc782b32f9dc5906bf48114791
Dan Ferg <89812588+Dawafe@users.noreply.github.com>
2021-11-05 11:50:02 -0600

    found an explanation/fix for my KeyError on stackoverflow

--- send_sms.py
+++ send_sms.py
@@ -5,8 +5,8 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['AC0f8a14d8a3f98dfd20a03453c51aba9b']
-auth_token = os.environ['252fc28c0245204dfc870f5c2645e915']
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
 message = client.messages \
