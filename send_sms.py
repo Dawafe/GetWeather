15208841b215dca0a109fde69a51173accecf605
Dan Ferg <89812588+Dawafe@users.noreply.github.com>
2021-11-05 12:10:43 -0600

    added #note on what fixed my issue

--- send_sms.py
+++ send_sms.py
@@ -5,6 +5,9 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
+#NOTE!- in order for this code to work I had to first rung this in terminal:
+# export TWILIO_ACCOUNT_SID=*my actual sid*
+# export TWILIO_AUTH_TOKEN=*my actual authtoken*
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
