ec568878d45087fe67cfec952346eaae38848355
Dan Ferg <89812588+Dawafe@users.noreply.github.com>
2021-11-05 11:00:29 -0600

    getting KeyError in python env, log out/in to twilio for diff auth tkn

--- send_sms.py
+++ send_sms.py
@@ -6,7 +6,7 @@ from twilio.rest import Client
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 account_sid = os.environ['AC0f8a14d8a3f98dfd20a03453c51aba9b']
-auth_token = os.environ['da65a8d2c3d7d15d95831dbd1dc2b93c']
+auth_token = os.environ['252fc28c0245204dfc870f5c2645e915']
 client = Client(account_sid, auth_token)
 
 message = client.messages \
