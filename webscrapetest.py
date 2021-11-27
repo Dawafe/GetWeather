9d6ae342b444a0b5f067c5228a818ada11c26e1a
Dan Ferg <89812588+Dawafe@users.noreply.github.com>
2021-11-26 19:44:16 -0700

    moving away from DataFrame use

--- webscrapetest.py
+++ webscrapetest.py
@@ -1,26 +1,19 @@
 #first, import all necessary libraries
 from bs4 import BeautifulSoup as bs
-import pandas as pd
-pd.set_option('display.max_colwidth', 500)
 import time
 import requests
-from pandas import DataFrame as df
 
-realfeel = []
-#list of urls
-urls = [f"https://www.accuweather.com/en/us/littleton/80120/weather-forecast/332178"]
-#iterating through URLs
-for url in urls:
+realfeel = ()
+url = (f"https://www.accuweather.com/en/us/littleton/80120/weather-forecast/332178")
+#scrapin' away
+def getwethr():
     #accessing the webpage
     page = requests.get(url)
     #getting the page's content in pure html
     soup = bs(page.content)
-    #adding the RealFeel temp to the dataframe
-    realfeel.extend([i.text for i in soup.find_all(class_='real-feel')])
-    #creating a dataframe to store scraped info
-    df = pd.DataFrame()
-    #storing the text in a dataframe
-    df['Temp'] = realfeel
-    print(df)
+    #getting RealFeel text results
+    results = soup.find_all(class_="real-feel")
+    #view results
+    print(results())
     #-DISABLED WHILE TESTING time.sleep(86400)
-    #-DISABLED WHILE TESTING print(df)
+    #-DISABLED WHILE TESTING print(results.prettify())
