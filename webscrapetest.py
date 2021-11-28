46b4ffad3b2f8e344c4a281389c395b345ebca4c
Dan Ferg <89812588+Dawafe@users.noreply.github.com>
2021-11-27 18:00:56 -0700

    Scraper works and gets current temp

--- webscrapetest.py
+++ webscrapetest.py
@@ -3,8 +3,7 @@ from bs4 import BeautifulSoup as bs
 import time
 import requests
 
-realfeel = ()
-url = (f"https://www.accuweather.com/en/us/littleton/80120/weather-forecast/332178")
+url = (f"https://weather.com/weather/today/l/443bba801902adfc2e7efa8237c7ba57ad62386f06a88b9bcbe00f39d1a7c22e")
 #scrapin' away
 def getwethr():
     #accessing the webpage
@@ -12,8 +11,10 @@ def getwethr():
     #getting the page's content in pure html
     soup = bs(page.content)
     #getting RealFeel text results
-    results = soup.find_all(class_="real-feel")
+    results = soup.find(class_="CurrentConditions--tempValue--3a50n")
     #view results
-    print(results())
+    print(results.text)
     #-DISABLED WHILE TESTING time.sleep(86400)
     #-DISABLED WHILE TESTING print(results.prettify())
+
+getwethr()
