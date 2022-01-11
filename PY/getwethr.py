d97a4c87dbc66513279c3beedf14989bdafdf95e
Dan Ferg <89812588+Dawafe@users.noreply.github.com>
2022-01-10 18:01:23 -0700

    Changed function output from print() to return

--- PY/getwethr.py
+++ PY/getwethr.py
@@ -1,10 +1,9 @@
-#first, import all necessary libraries
-# Download the helper library from https://www.twilio.com/docs/python/install
-# STEP 1. Must use virtual env for this. run- source getwethr_env/bin/activate
-# Find your Account SID and Auth Token at twilio.com/console
-# STEP 2. in order for this code to work I had to first run this in terminal:
+# STEP 1. in order for this code to work I had to first run this in terminal:
 # export TWILIO_ACCOUNT_SID=*my actual sid*
 # export TWILIO_AUTH_TOKEN=*my actual auth token*
+# STEP 2. Must use virtual env for this. run- source getwethr_env/bin/activate
+# Find your Account SID and Auth Token at twilio.com/console
+
 import os
 from twilio.rest import Client
 from bs4 import BeautifulSoup as bs
@@ -21,11 +20,12 @@ def getwethr():
     #getting RealFeel text results
     results = soup.find(class_="CurrentConditions--tempValue--3a50n")
     #view results
-    print(results.text)
+    x = str(results.text)
+    return x
     #-DISABLED WHILE TESTING time.sleep(86400)
     #-DISABLED WHILE TESTING print(results.prettify())
 
-getwethr()
+y = getwethr()
 
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
@@ -33,7 +33,7 @@ client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
-                     body='',
+                     body=y,
                      from_='+18506018836',
                      to='+17145590485'
                  )
